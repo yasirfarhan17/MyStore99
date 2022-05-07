12f5bfb49f7cb7157d37298d49e12f06659e4775
user <yasirfarhan7@gmail.com>
2022-05-07 23:19:10 +0530

    login page

---
+++ app/src/main/java/com/noor/mystore99/amigrate/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.noor.mystore99.amigrate.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noor.mystore99.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
