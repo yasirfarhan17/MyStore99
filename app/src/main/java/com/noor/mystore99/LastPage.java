95781838c50b20ef83ad5ce6175d693343a74206
sahilparwez <sahilfar135@gmail.com>
2026-01-02 00:39:18 +0530

    feat: Bump minSdkVersion to 23 and refactor code
    Increased the `minSdkVersion` for the `NetworkModule` from 21 to 23.
    
    Also includes minor code refactoring:
    - Corrected a Firebase database reference path in `LastPage.java`.
    - Changed `SliderModel` class access modifier from public to package-private.

--- app/src/main/java/com/noor/mystore99/LastPage.java
+++ app/src/main/java/com/noor/mystore99/LastPage.java
@@ -266,8 +266,7 @@ public class LastPage extends AppCompatActivity {
         });
 
 
-        ref= FirebaseDatabase.getInstance().getReference("Myorder").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Item
-").child("YourOrder").child(zz);
+        ref= FirebaseDatabase.getInstance().getReference("Myorder").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Item ").child("YourOrder").child(zz);
         m.clear();
         //Toast.makeText(LastPage.this,pp+" "+quant,Toast.LENGTH_SHORT).show();
         ref.addChildEventListener(new ChildEventListener() {
