a524718c3d5744c632511296f0bc9a10509d56e4
sahilparwez <sahilfar135@gmail.com>
2026-01-02 20:02:12 +0530

    refactor: Remove unused Hilt dependencies
    Removes the `hilt-lifecycle-viewmodel` and `hilt-compiler` dependencies from the NetworkModule, as they are no longer needed.
    
    Also changes the `sliderModel` class access modifier from public to package-private.

---
+++ app/src/main/java/com/noor/mystore99/amigrate/ui/about/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.noor.mystore99.amigrate.ui.about
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.noor.mystore99.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_page)
+    }
+}
